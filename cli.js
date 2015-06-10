a0c686e2a48ff4ac8fd177eee790a55a4b1b8d51
Johannes J. Schmidt <jo@die-tf.de>
2015-06-10 12:41:44 +0200

    Fic CLI argument processing

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@
 var compile = require('./')
 
 var args = process.argv.slice(2)
-var source = args[2] || process.cwd()
+var source = args[0] || process.cwd()
 
 compile(source, function(error, response) {
   if (error) return console.error(error)
